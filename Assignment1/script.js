b11653fc130ef3898f576f6ec047e13f8f0a9132
Mandeep Singh <ms54994@gmail.com>
2020-06-04 17:25:38 +0530

    done

--- Assignment1/script.js
+++
@@ -1,6 +0,0 @@
-let csvToJson = require('convert-csv-to-json');
- 
-let fileInputName = 'customer-data.csv'; 
-let fileOutputName = 'myOutputFile.json';
- 
-csvToJson.fieldDelimiter(',').generateJsonFileFromCsv(fileInputName,fileOutputName);
